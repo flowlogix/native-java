ca85bf013e46e6f6903694e7e4b7787f3e46d0d4
lprimak <lenny@flowlogix.com>
2023-11-22 18:56:42 -0600

    agent

--- src/main/java/com/flowlogix/example/HelloEntryPoint.java
+++ src/main/java/com/flowlogix/example/HelloEntryPoint.java
@@ -6,6 +6,7 @@ import org.jboss.weld.environment.se.Weld;
 public class HelloEntryPoint {
     public static void printHello(String name) {
         try (var _ = new Weld().addBeanClasses(HelloPrinter.class, HelloGreeter.class).disableDiscovery().initialize()) {
+//        try (var _ = new Weld().initialize()) {
             var helloPrinter = CDI.current().select(HelloPrinter.class).get();
             helloPrinter.printHello(name);
         }
