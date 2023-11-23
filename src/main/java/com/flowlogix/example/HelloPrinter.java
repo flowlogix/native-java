1ea96a088edd1355989a20886af3123f497e879e
lprimak <lenny@flowlogix.com>
2023-11-22 18:58:13 -0600

    remove sleep

--- src/main/java/com/flowlogix/example/HelloPrinter.java
+++ src/main/java/com/flowlogix/example/HelloPrinter.java
@@ -2,17 +2,13 @@ package com.flowlogix.example;
 
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import lombok.SneakyThrows;
-import java.time.Duration;
 
 @ApplicationScoped
 public class HelloPrinter {
     @Inject
     Greeter greeter;
 
-    @SneakyThrows(InterruptedException.class)
     public void printHello(String name) {
         System.out.println(greeter.greet(name));
-        Thread.sleep(Duration.ofSeconds(10));
     }
 }
