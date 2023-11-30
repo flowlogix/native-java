f78dca87b4402e2e6bfc0b970ab571fcd305248e
lprimak <lenny@flowlogix.com>
2023-11-30 10:27:56 -0600

    helidon: removed server parts

--- helidon/custom-mp/src/main/java/com/flowlogix/example/HelloEntryPoint.java
+++ helidon/custom-mp/src/main/java/com/flowlogix/example/HelloEntryPoint.java
@@ -10,7 +10,7 @@ public class HelloEntryPoint {
 
     public static void main(String[] args) {
         io.helidon.Main.main(args);
-        printHello("Anya");
+        printHello(args[0]);
         System.exit(0);
     }
 }
