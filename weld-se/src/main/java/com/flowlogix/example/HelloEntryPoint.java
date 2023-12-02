4b97f9f18de476017b33d7cbf259cdab1a2ecf75
lprimak <lenny@flowlogix.com>
2023-12-02 16:20:59 -0600

    even simpler weld initialization

--- weld-se/src/main/java/com/flowlogix/example/HelloEntryPoint.java
+++ weld-se/src/main/java/com/flowlogix/example/HelloEntryPoint.java
@@ -5,11 +5,7 @@ import jakarta.enterprise.inject.se.SeContainerInitializer;
 import jakarta.enterprise.inject.spi.CDI;
 
 public class HelloEntryPoint {
-    private static final SeContainer container;
-
-    static {
-        container = SeContainerInitializer.newInstance().initialize();
-    }
+    private static final SeContainer container = SeContainerInitializer.newInstance().initialize();
 
     public static void printHello(String name) {
         var helloPrinter = CDI.current().select(HelloPrinter.class).get();
