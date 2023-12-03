a77d067b6f491389582138f56517e86eb3bcccf4
lprimak <lenny@flowlogix.com>
2023-12-03 00:17:22 -0600

    added db to quarkus

--- quarkus/command-mode-quickstart/src/main/java/com/flowlogix/example/HelloGreeter.java
+++ quarkus/command-mode-quickstart/src/main/java/com/flowlogix/example/HelloGreeter.java
@@ -1,11 +1,21 @@
 package com.flowlogix.example;
 
+import com.flowlogix.example.data.entities.Sample;
 import jakarta.enterprise.context.Dependent;
+import jakarta.inject.Inject;
+import jakarta.persistence.EntityManager;
+import jakarta.transaction.Transactional;
 
 @Dependent
 public class HelloGreeter implements Greeter {
+    @Inject
+    EntityManager em;
+
     @Override
+    @Transactional
     public String greet(String name) {
-        return "Hello %s!".formatted(name);
+        Sample birthday = em.createNamedQuery("Sample.findAll", Sample.class).setMaxResults(1).getResultStream()
+                .findFirst().get();
+        return "Hello %s! - your birthday is %s".formatted(name, birthday.getDateOfBirth());
     }
 }
