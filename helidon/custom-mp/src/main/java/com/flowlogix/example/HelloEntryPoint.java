cc507fb13d1eeb3b7fdc02748fd96a6bed604c7c
lprimak <lenny@flowlogix.com>
2023-12-07 15:40:34 -0600

    incorporating FL SE Native

--- helidon/custom-mp/src/main/java/com/flowlogix/example/HelloEntryPoint.java
+++ helidon/custom-mp/src/main/java/com/flowlogix/example/HelloEntryPoint.java
@@ -1,11 +1,13 @@
 package com.flowlogix.example;
 
 import com.flowlogix.cmdline.CommandLine;
+import com.flowlogix.weld.graalvm.DynamicWeld;
 import io.helidon.microprofile.cdi.Main;
 import jakarta.enterprise.inject.spi.CDI;
 
 public class HelloEntryPoint {
     public static void main(String[] args) {
+        DynamicWeld.initialize();
         Main.main(args);
         CommandLine.setArguments(args).run(CDI.current().select(HelloPrinter.class).get()::printHello);
     }
