8bb8510a5f8e4788ab38c12b4af7d4903beaec8e
lprimak <lenny@flowlogix.com>
2023-12-09 02:39:20 -0600

    helidon db

---
+++ helidon/helidon-greeter/src/main/java/com/flowlogix/example/EntityProducer.java
@@ -0,0 +1,14 @@
+package com.flowlogix.example;
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.inject.Produces;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import lombok.Getter;
+
+@RequestScoped
+public class EntityProducer {
+    @Getter(onMethod = @__(@Produces))
+    @PersistenceContext
+    EntityManager entityManager;
+}
--- helidon/helidon-greeter/src/main/java/com/flowlogix/example/HelloEntryPoint.java
+++ helidon/helidon-greeter/src/main/java/com/flowlogix/example/HelloEntryPoint.java
@@ -4,13 +4,25 @@ import com.flowlogix.bootstrap.cmdline.CommandLine;
 import com.flowlogix.examples.greeter.HelloPrinter;
 import com.flowlogix.weld.graalvm.DynamicWeld;
 import io.helidon.microprofile.cdi.Main;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.inject.spi.CDI;
+import jakarta.inject.Inject;
+import org.jboss.weld.context.activator.ActivateRequestContext;
 
+@ApplicationScoped
 public class HelloEntryPoint {
+    @Inject
+    CommandLine cmdLine;
+    @Inject HelloPrinter helloPrinter;
+
     public static void main(String[] args) {
         DynamicWeld.initialize();
         Main.main(args);
-        CDI.current().select(CommandLine.class).get().setArguments(args)
-                .run(CDI.current().select(HelloPrinter.class).get()::printHello);
+        CDI.current().select(HelloEntryPoint.class).get().entryPoint(args);
+    }
+
+    @ActivateRequestContext
+    void entryPoint(String[] args) {
+        cmdLine.setArguments(args).run(helloPrinter::printHello);
     }
 }
