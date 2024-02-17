d243290abe04260f8690ef60ea70b7e9d67fa492
lprimak <lenny@flowlogix.com>
2024-02-16 19:46:10 -0600

    enh: checkstyle conformance

--- weld-se-greeter/src/main/java/com/flowlogix/example/HelloEntryPoint.java
+++ weld-se-greeter/src/main/java/com/flowlogix/example/HelloEntryPoint.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2011-2024 Flow Logix, Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.flowlogix.example;
 
 import com.flowlogix.bootstrap.cmdline.CommandLine;
@@ -8,9 +23,11 @@ import jakarta.enterprise.inject.spi.CDI;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 public class HelloEntryPoint {
-    @SuppressWarnings("unused")
+    @SuppressWarnings({"unused", "checkstyle:ConstantName"})
     private static final Void loggerInit = LoggingStartup.init();
+    @SuppressWarnings("checkstyle:ConstantName")
     private static final SeContainer container = SeContainerInitializer.newInstance().initialize();
 
     public static void entry(String... args) {
